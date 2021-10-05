5990ca0ed4c29b9426f5abf177c5166e565d0268
DAVID Jean-Francois <jean-francois.david@inria.fr>
2021-10-05 15:12:31 +0200

    ajout du 2eme noyau cuda + nettoyage code

--- test/starpu_cuda_cudnn/kernels_cudnn.cu
+++ test/starpu_cuda_cudnn/kernels_cudnn.cu
@@ -6,13 +6,13 @@ static __global__ void cuda_dev_const(float *px, float k)
         px[tid] = k;
 }
 
-//static __global__ void cuda_dev_iota(float *px)
-//{
-//        int tid = threadIdx.x + blockIdx.x * blockDim.x;
-//        px[tid] = tid;
-//}
+static __global__ void cuda_dev_iota(float *px)
+{
+        int tid = threadIdx.x + blockIdx.x * blockDim.x;
+        px[tid] = tid;
+}
 
-extern "C" void cuda_dev_const(void *descr[], void *_args)
+extern "C" void dev_const(void *descr[], void *_args)
 {
         float *filt_data = (float *)STARPU_MATRIX_GET_PTR(descr[0]);
 
@@ -26,15 +26,15 @@ extern "C" void cuda_dev_const(void *descr[], void *_args)
         cudaStreamSynchronize(starpu_cuda_get_local_stream());
 }
 
-//extern "C" void cuda_dev_iota(void *descr[], void *_args)
-//{
-//        float *filt_data = (float *)STARPU_BLOCK_GET_PTR(descr[0]);
-//        int in_size = STARPU_BLOCK_GET_NX(descr[0]);
-//        int in_number = STARPU_BLOCK_GET_NY(descr[0]);
-// 
-//        cuda_block<<<in_size, in_number, 0, starpu_cuda_get_local_stream()>>>(filt_data);
-//
-//        cudaError_t status = cudaGetLastError();
-//        if (status != cudaSuccess) STARPU_CUDA_REPORT_ERROR(status);
-//        cudaStreamSynchronize(starpu_cuda_get_local_stream());
-//}
+extern "C" void dev_iota(void *descr[], void *_args)
+{
+        float *in_data = (float *)STARPU_MATRIX_GET_PTR(descr[0]);
+        int in_size = STARPU_MATRIX_GET_NX(descr[0]);
+        int in_nb = STARPU_MATRIX_GET_NY(descr[0]);
+ 
+        cuda_dev_iota<<<in_size, in_nb, 0, starpu_cuda_get_local_stream()>>>(in_data);
+
+        cudaError_t status = cudaGetLastError();
+        if (status != cudaSuccess) STARPU_CUDA_REPORT_ERROR(status);
+        cudaStreamSynchronize(starpu_cuda_get_local_stream());
+}
--- test/starpu_cuda_cudnn/main_cudnn.c
+++ test/starpu_cuda_cudnn/main_cudnn.c
@@ -3,17 +3,17 @@
 
 #define FPRINTF(ofile, fmt, ...) do { if (!getenv("STARPU_SSILENT")) {fprintf(ofile, fmt, ## __VA_ARGS__); }} while(0)
 
-extern void cuda_dev_const(void *descr[], void *_args);
+extern void dev_const(void *descr[], void *_args);
+extern void dev_iota(void *descr[], void *_args);
 
 typedef void (*device_func)(void **, void *);
 
-int execute_on(uint32_t where, device_func func, float *filt_data, int pnx, int pny)
+int execute_on(uint32_t where, device_func func, float *matrix_data, int pnx, int pny)
 {
   struct starpu_codelet cl;
-  starpu_data_handle_t filt_data_handle;
-  int i;
+  starpu_data_handle_t matrix_data_handle;
 
-  starpu_matrix_data_register(&filt_data_handle, STARPU_MAIN_RAM, (uintptr_t)filt_data, pnx, pnx, pny, sizeof(float));
+  starpu_matrix_data_register(&matrix_data_handle, STARPU_MAIN_RAM, (uintptr_t)matrix_data, pnx, pnx, pny, sizeof(float));
 
   starpu_codelet_init(&cl);
   cl.where = where;
@@ -21,17 +21,16 @@ int execute_on(uint32_t where, device_func func, float *filt_data, int pnx, int 
   cl.nbuffers = 1;
   cl.modes[0] = STARPU_RW,
   cl.model = NULL;
-  cl.name = "dev_const";
+  cl.name = "dev";
 
   struct starpu_task *task = starpu_task_create();
   task->cl = &cl;
   task->callback_func = NULL;
-  task->handles[0] = filt_data_handle;
+  task->handles[0] = matrix_data_handle;
 
   int ret = starpu_task_submit(task);
   if (STARPU_UNLIKELY(ret == -ENODEV))
   {
-    printf("\n\n");
     FPRINTF(stderr, "No worker may execute this task\n");
     task->destroy = 0;
     starpu_task_destroy(task);
@@ -39,20 +38,32 @@ int execute_on(uint32_t where, device_func func, float *filt_data, int pnx, int 
   }
 
   starpu_task_wait_for_all();
+  starpu_data_unregister(matrix_data_handle);
 
-  /* update the array in RAM */
-  starpu_data_unregister(filt_data_handle);
+  for(int i=0; i<pnx*pny; i++)
+  {
+    FPRINTF(stderr, "%f ", matrix_data[i]);
+  }
+  FPRINTF(stderr, "\n\n");
+
 
   return 0;
 }
 
+
 int main(void)
 {
   const int filt_k = 1, filt_c = 1, filt_h = 2, filt_w = 2;
-  float *filt_data;
+  const int in_n = 1, in_c = 1, in_h = 5, in_w = 5;
+
+  float *filt_data, *in_data;
   int ret;
-  int nx=filt_w * filt_h;
-  int ny=filt_k * filt_c;
+
+  int cosnt_nx=filt_w * filt_h;
+  int const_ny=filt_k * filt_c;
+
+  int iota_nx = in_w * in_h;
+  int iota_ny = in_n * in_c;
 
   //Init StarPu
   ret = starpu_init(NULL);
@@ -60,12 +71,15 @@ int main(void)
     return 77;
   STARPU_CHECK_RETURN_VALUE(ret, "starpu_init");
 
-
-  filt_data = (float*)malloc(nx*ny*sizeof(float));
+  filt_data = (float*)malloc(cosnt_nx*const_ny*sizeof(float));
   assert(filt_data);
+  execute_on(STARPU_CUDA, dev_const, filt_data, cosnt_nx, const_ny);
 
-  ret = execute_on(STARPU_CUDA, cuda_dev_const, filt_data, nx, ny);
+  in_data = (float*)malloc(iota_nx * iota_ny * sizeof(float));
+  assert(in_data);
+  execute_on(STARPU_CUDA, dev_iota, in_data, iota_nx, iota_ny);
 
+  free(in_data);
   free(filt_data);
   starpu_shutdown();
 
